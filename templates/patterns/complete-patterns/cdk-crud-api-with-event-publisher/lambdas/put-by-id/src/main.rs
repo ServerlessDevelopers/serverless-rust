1f2b88bba41888d393129879a940b6d657808a74
James Eastham <dev@jameseastham.co.uk>
2024-05-01 18:27:03 +0100

    Simplify put handler

--- templates/patterns/complete-patterns/cdk-crud-api-with-event-publisher/lambdas/put-by-id/src/main.rs
+++ templates/patterns/complete-patterns/cdk-crud-api-with-event-publisher/lambdas/put-by-id/src/main.rs
@@ -1,6 +1,7 @@
 use lambda_http::{Error, IntoResponse, Request, RequestPayloadExt, Response, run, service_fn};
+use lambda_http::http::StatusCode;
 
-use shared::{ApplicationError, OrderRepository, UpdateOrderCommand, UpdateOrderCommandHandler};
+use shared::{ApplicationError, CreateOrderCommand, OrderRepository, UpdateOrderCommand, UpdateOrderCommandHandler};
 
 #[tokio::main]
 async fn main() -> Result<(), Error>{
@@ -24,49 +25,37 @@ async fn main() -> Result<(), Error>{
 }
 
 async fn handler(command_handler: &UpdateOrderCommandHandler, event: Request) -> Result<impl IntoResponse, Error> {
-    let body = event.payload::<UpdateOrderCommand>();
+    let item = event.payload::<UpdateOrderCommand>()?;
 
-    match body {
-        Ok(item) => {
-            match item {
-                Some(i) => {
-                    let handler_result = command_handler.handle(i).await;
+    let resp = match item {
+        Some(i) => {
+            let handler_result = command_handler.handle(i).await;
 
-                    let resp = match handler_result {
-                        Ok(order) => {
-                            let serde_model = serde_json::to_string(&order)?;
-                            Response::builder()
-                                .status(200)
-                                .header("content-type", "application/json")
-                                .body(serde_model)
-                                .map_err(Box::new)?
-                        }
-                        Err(_) => Response::builder()
-                            .status(400)
-                            .header("content-type", "application/json")
-                            .body("".to_string())
-                            .map_err(Box::new)?
-                    };
-
-                    Ok(resp)
+            let resp = match handler_result {
+                Ok(order) => {
+                    let serde_model = serde_json::to_string(&order)?;
+                    generate_response(201, serde_model)
                 }
-                None => {
-                    let resp = Response::builder()
-                        .status(400)
-                        .header("content-type", "text/json")
-                        .body("".to_string())
-                        .map_err(Box::new)?;
-                    Ok(resp)
-                }
-            }
+                Err(_) => generate_response(500, "".to_string())
+            };
+            resp
         }
-        Err(e) => {
-            let resp = Response::builder()
-                .status(400)
-                .header("content-type", "text/json")
-                .body(e.to_string())
-                .map_err(Box::new)?;
-            Ok(resp)
+        None => {
+            let resp = generate_response(400, "".to_string());
+
+            resp
         }
-    }
+    };
+
+    resp
+}
+
+fn generate_response(status: u16, body: String) -> Result<Response<String>, Error> {
+    let response = Response::builder()
+        .status(StatusCode::from_u16(status).unwrap())
+        .header("content-type", "application/json")
+        .body(body)
+        .map_err(Box::new)?;
+
+    Ok(response)
 }
